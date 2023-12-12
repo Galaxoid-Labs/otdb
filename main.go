23b57858444eda9b1c2b3a22d9e7d4e97bc5f591
Jacob Davis <ismyhc@gmail.com>
2023-12-12 16:56:39 -0500

    mongo stuff

--- main.go
+++ main.go
@@ -1,31 +1,91 @@
 package main
 
 import (
+	"context"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"reflect"
 	"strconv"
 	"sync"
+	"time"
 
 	"github.com/fxamacker/cbor/v2"
 	"github.com/hashicorp/go-retryablehttp"
 	"github.com/nozzle/throttler"
 	"github.com/schollz/progressbar/v3"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const ordHost = "http://38.99.82.238:8080"
 
 var (
-	retryClient    = retryablehttp.NewClient()
-	client         = retryClient.StandardClient()
-	inscriptionIDs []string
-	inscriptions   = make(map[string]InscriptionExtended)
-	batchSize      = 100    // Much higher than this and it actually goes slower. About 7 seconds for 3000 inscriptions, 1.50 seconds for 3000 sats
-	block          = 775492 // 767430
+	retryClient            = retryablehttp.NewClient()
+	client                 = retryClient.StandardClient()
+	inscriptionIDs         []string
+	inscriptions           = make(map[string]InscriptionExtended)
+	batchSize              = 100
+	block                  = 775492 // 767430
+	mongoConnection        = os.Getenv("MONGO_CONNECTION")
+	mongoClient            *mongo.Client
+	mongoCtx               = context.TODO()
+	inscriptionsCollection *mongo.Collection
 )
+
+func ConnectMongoDB() {
+	fmt.Println("Connecting to MongoDB")
+	mongoOptions := options.Client().ApplyURI(mongoConnection)
+
+	var err error
+	mongoClient, err = mongo.Connect(mongoCtx, mongoOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	err = mongoClient.Ping(mongoCtx, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	inscriptionsCollection = mongoClient.Database("ord").Collection("inscriptions")
+	if inscriptionsCollection == nil {
+		panic("inscriptionsCollection is nil")
+	}
+	_, err = inscriptionsCollection.Indexes().CreateOne(mongoCtx, mongo.IndexModel{
+		Keys:    bson.D{{Key: "inscription_id", Value: 1}},
+		Options: options.Index().SetUnique(true),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = inscriptionsCollection.Indexes().CreateOne(mongoCtx, mongo.IndexModel{
+		Keys:    bson.D{{Key: "sat_rarity", Value: 1}},
+		Options: options.Index().SetUnique(false),
+	})
+	if err != nil {
+		panic(err)
+	}
+	_, err = inscriptionsCollection.Indexes().CreateOne(mongoCtx, mongo.IndexModel{
+		Keys:    bson.D{{Key: "content_type", Value: "text"}, {Key: "content", Value: "text"}, {Key: "meta_protocol", Value: "text"}},
+		Options: options.Index().SetUnique(false),
+	})
+	if err != nil {
+		panic(err)
+	}
+	_, err = inscriptionsCollection.Indexes().CreateOne(mongoCtx, mongo.IndexModel{
+		Keys:    bson.D{{Key: "metadata.$**", Value: 1}},
+		Options: options.Index().SetUnique(false),
+	})
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	retryClient.Logger = nil
@@ -33,6 +93,8 @@ func main() {
 
 	fmt.Println("Starting Ord Scraper")
 	fmt.Printf("Using Host: %v\n", ordHost)
+
+	ConnectMongoDB()
 
 	highestBlock, err := getHighestBlock()
 	if err != nil {
@@ -42,8 +104,12 @@ func main() {
 
 	// get last block written to db and start from there
 	// We will always overwrite the last block in the db so we don't need to worry about missing any
-	getAndSetStartingBlockFromDB()
-	fmt.Printf("Starting at block: %v\n", block)
+	lastBlockWritten := getAndSetStartingBlockFromDB()
+	if lastBlockWritten != block {
+		block = lastBlockWritten
+		deleteInscriptionsFromBlock()
+	}
+	fmt.Printf("Starting at block: %v\n\n", block)
 
 	for block <= highestBlock {
 
@@ -98,7 +164,6 @@ func getAndWriteInscriptions() {
 		}(id)
 		t.Throttle()
 	}
-	fmt.Println("")
 }
 
 func getHighestBlock() (int, error) {
@@ -130,7 +195,7 @@ func getBlock(height int, page int) (*Block, error) {
 }
 
 func getInscription(inscriptionID string) (*InscriptionExtended, error) {
-	body, err := makeRequest(fmt.Sprintf("%s/inscription/%v", ordHost, inscriptionID))
+	body, err := makeRequest(fmt.Sprintf("%s/e/inscription/%v", ordHost, inscriptionID))
 	if err != nil {
 		return nil, err
 	}
@@ -160,16 +225,76 @@ func makeRequest(url string) ([]byte, error) {
 }
 
 func convertAndWriteInscriptionsToDB() {
+	ins := []DBInscription{}
+	for _, v := range inscriptions {
+		ins = append(ins, DBInscription{
+			// Rune:              v.Rune,
+			Address:           v.Address,
+			Children:          v.Children,
+			ContentLength:     v.ContentLength,
+			ContentType:       v.ContentType,
+			GenesisFee:        v.GenesisFee,
+			GenesisHeight:     v.GenesisHeight,
+			InscriptionID:     v.InscriptionID,
+			InscriptionNumber: strconv.Itoa(v.InscriptionNumber),
+			Next:              v.Next,
+			OutputValue:       v.OutputValue,
+			Parent:            v.Parent,
+			Previous:          v.Previous,
+			Sat:               strconv.FormatUint(v.Sat, 10),
+			Timestamp:         time.Unix(int64(v.Timestamp), 0),
+			Satpoint:          v.Satpoint,
+			CharmsExtended:    v.CharmsExtended,
+			SatRarity:         v.SatRarity,
+			MetadataHex:       v.MetadataHex,
+			Metadata:          v.Metadata,
+			MetaProtocol:      v.MetaProtocol,
+			ContentEncoding:   v.ContentEncoding,
+			Content:           v.Content,
+		})
+	}
+
+	var coll []interface{}
+	for _, s := range ins {
+		coll = append(coll, s)
+	}
+
+	_, err := inscriptionsCollection.InsertMany(mongoCtx, coll, options.InsertMany().SetOrdered(false))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("Wrote %v inscriptions to DB for block: %v\n", len(inscriptions), block)
+	fmt.Println("")
+}
+
+func deleteInscriptionsFromBlock() {
+	fmt.Printf("Deleting inscriptions from last written block: %v\n", block)
+	_, err := inscriptionsCollection.DeleteMany(mongoCtx, bson.M{"genesis_height": block})
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func getAndSetStartingBlockFromDB() int {
-	return 0
+	v, err := inscriptionsCollection.Find(mongoCtx, bson.M{}, options.Find().SetSort(bson.D{{Key: "genesis_height", Value: -1}}).SetLimit(1))
+	if err != nil {
+		return block
+	}
+	var results []DBInscription
+	if err = v.All(mongoCtx, &results); err != nil {
+		return block
+	}
+	if len(results) > 0 {
+		return int(results[0].GenesisHeight)
+	}
+	return block
 }
 
-func decodeMetadata(v string) (*string, error) {
+func decodeMetadata(v string) string {
 	data, err := hex.DecodeString(v)
 	if err != nil {
-		return nil, err
+		return ""
 	}
 	opts := cbor.DecOptions{
 		MaxArrayElements: 65535,
@@ -180,28 +305,28 @@ func decodeMetadata(v string) (*string, error) {
 
 	d, err := opts.DecMode()
 	if err != nil {
-		return nil, err
+		return ""
 	}
 
 	var metadata interface{}
 	err = d.Unmarshal(data, &metadata)
 	if err != nil {
-		return nil, err
+		return ""
 	}
 
 	jsonStr, err := json.Marshal(metadata)
 	if err != nil {
-		return nil, err
+		return ""
 	}
 
 	escaped, err := json.Marshal(string(jsonStr))
 	if err != nil {
-		return nil, err
+		return ""
 	}
 
 	strEscaped := string(escaped)
 
-	return &strEscaped, nil
+	return strEscaped
 }
 
 // func formatJSON(data []byte) string {
@@ -253,40 +378,31 @@ type Charm struct {
 	Icon  string `json:"icon"`
 }
 
-type Sat struct {
-	Number       uint64   `json:"number"`
-	Decimal      string   `json:"decimal"`
-	Degree       string   `json:"degree"`
-	Name         string   `json:"name"`
-	Block        int      `json:"block"`
-	Cycle        int      `json:"cycle"`
-	Epoch        int      `json:"epoch"`
-	Period       int      `json:"period"`
-	Offset       int      `json:"offset"`
-	Rarity       string   `json:"rarity"`
-	Percentile   string   `json:"percentile"`
-	Satpoint     string   `json:"satpoint"`
-	Timestamp    int      `json:"timestamp"`
-	Inscriptions []string `json:"inscriptions"`
+type DBInscription struct {
+	// ID primitive.ObjectID `bson:"_id"`
+	// CreatedAt time.Time `bson:"created_at"`
+	// UpdatedAt time.Time `bson:"updated_at"`
+	// Rune              string   `bson:"rune"` // uint128
+	Address           string                 `bson:"address"`
+	Children          []string               `bson:"children"`
+	ContentLength     int                    `bson:"contentLength"`
+	ContentType       string                 `bson:"contentType"`
+	GenesisFee        int                    `bson:"genesisFee"`
+	GenesisHeight     uint32                 `bson:"genesisHeight"`
+	InscriptionID     string                 `bson:"inscriptionId"`
+	InscriptionNumber string                 `bson:"inscriptionNumber"`
+	Next              string                 `bson:"next"`
+	OutputValue       int                    `bson:"outputValue"`
+	Parent            string                 `bson:"parent,omitempty"`
+	Previous          string                 `bson:"previous"`
+	Sat               string                 `bson:"sat,omitempty"`
+	Satpoint          string                 `bson:"satpoint,omitempty"`
+	Timestamp         time.Time              `bson:"timestamp"`
+	CharmsExtended    []Charm                `bson:"charmsExtended"` // Decoded charms with title and icon emoji
+	SatRarity         string                 `bson:"satRarity"`
+	MetadataHex       string                 `bson:"metadataHex"` // CBOR encoded. Decoded on conversion to DB type
+	Metadata          map[string]interface{} `bson:"metadata"`    // CBOR decoded json object
+	MetaProtocol      string                 `bson:"metaprotocol"`
+	ContentEncoding   string                 `bson:"contentEncoding"`
+	Content           string                 `bson:"content"` // Escaped string which could be JSON, Markdown or plain text. Null otherwise
 }
-
-// type DBInscription struct {
-// 	ID        primitive.ObjectID `bson:"_id"`
-// 	CreatedAt time.Time          `bson:"created_at"`
-// 	UpdatedAt time.Time          `bson:"updated_at"`
-// 	Address   string             `bson:"address"`
-// 	Children  []string           `bson:"children"`
-// 	// ContentLength     int                `bson:"content_length"`
-// 	// ContentType       string             `bson:"content_type"`
-// 	GenesisFee        int    `bson:"genesis_fee"`
-// 	GenesisHeight     uint32 `bson:"genesis_height"`
-// 	InscriptionID     string `bson:"inscription_id"`
-// 	InscriptionNumber int    `bson:"inscription_number"`
-// 	Next              string `bson:"next"`
-// 	OutputValue       int    `bson:"output_value"`
-// 	Parent            string `bson:"parent,omitempty"`
-// 	Previous          string `bson:"previous"`
-// 	Rune              string `bson:"rune"` // uint128
-// 	Sat               uint64 `bson:"sat,omitempty"`
-// 	Satpoint          string `bson:"satpoint,omitempty"`
-// }
